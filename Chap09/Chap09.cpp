bbf5437f85a1d5c5c82443cdf846145832ca7d28
YUHAN <YUHAN@PC7213_09>
2022-04-28 16:49:22 +0900

    초기 커밋.

---
+++ Chap09/Chap09.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+class Node
+{
+public:
+    Node(int input)
+    {
+        data = input;
+        next = nullptr;
+    }
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    Node* start = new Node(1);
+    Node* current = start;
+    current->next = new Node(2);
+    current = current->next;
+    current->next = new Node(3);
+    current = current->next;
+
+    current = start;
+
+    while (current != nullptr)
+    {
+        std::cout << current->data << std::endl;
+        current = current->next;
+
+    }
+
+}
